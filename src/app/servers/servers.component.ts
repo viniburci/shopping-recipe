dd8cb22a66e4476c88f95590598f2d4884ae495a
Vinicius Burci <vpburci@gmail.com>
2023-08-26 04:48:18 -0300

    componentes gerados, estrutura geral organizada

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
 
+  serverId = 10;
+  serverStatus = 'offline';
+  serverName = '';
+  serverCreated = false;
+  servers = ['Servidor 1', 'Servidor teste']
+
+  allowNewServer = false;
+
+  constructor () {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+    this.serverStatus = Math.random() > 0.5? 'online': 'offline';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onCreateServer() {
+    this.serverStatus = 'online';
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+
+  }
+
+  getColor(){
+    return this.serverStatus === 'online'? 'green': 'red';
+  }
 }
