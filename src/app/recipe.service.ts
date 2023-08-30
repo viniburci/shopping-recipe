14c3c952c93c8e71777018aced7c43f93ab1bfd9
Vinicius Burci <vpburci@gmail.com>
2023-08-30 15:04:46 -0300

    display recipe ingredients at recipe-details

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipes/recipe.model';
+import { Ingredient } from './shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +12,27 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'Recipe description',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+      'Ratatouille',
+      'Legumes assados',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+      [
+        new Ingredient('Abobrinha', 1),
+        new Ingredient('Beringela', 1),
+        new Ingredient('Azeite', 1),
+        new Ingredient('Cebola', 1/2),
+        new Ingredient('Tomate', 5),
+        new Ingredient('Alecrim', 1),
+        new Ingredient('Pimenta-do-reino', 1),
+        new Ingredient('Manjericao', 1),
+      ]
     ),
     new Recipe(
       'A test recipe 2',
       'Recipe description 2',
-      'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*'
+      'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*',
+      [
+        new Ingredient('Tomato', 2),
+      ]
     ),
   ];
 
